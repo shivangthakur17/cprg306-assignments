c0ea99ec40b27018872ca3db1bf85fb3f9e7333e
shivangthakur17 <shivangthakur2006@gmail.com>
2026-01-22 13:35:47 -0700

    changes in page.js
    import studentinfo

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import StudentInfo from './week-2/StudentInfo';
 export default function Page() {
   return (
     <main>
-      <h1>Shopping List</h1>
+      <h1>Week-2</h1>
+      <StudentInfo />
     </main>
   );
 }
