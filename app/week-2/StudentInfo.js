a0f42a8597c56584a34f712ccb0ab50ceb217047
shivangthakur17 <shivangthakur2006@gmail.com>
2026-01-22 13:12:52 -0700

    changed page.js and studentinfo.js

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Shivang Thakur</h1>
+
+      <p>
+        <Link href="https://github.com/shivangthakur17/cprg306-assignments.git">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
