a3d5bfce3cfc05c0f6d0964384864559c44899dd
shivangthakur17 <shivangthakur2006@gmail.com>
2026-03-05 20:06:30 -0700

    added folders for week 8, 9, 10

--- app/page.js
+++ app/page.js
@@ -7,11 +7,11 @@ export default function Page() {
       dark:bg-black dark:text-purple-200">
 
       {/* Top-left name */}
-      <div className="absolute top-4 left-4 font-semibold size-base text-purple-600 dark:text-purple-300">
+      <div className="absolute top-4 left-4 font-semibold size-base text-purple-800 dark:text-purple-900">
         Shivang Thakur
       </div>
 
-      <div className="bg-white dark:bg-purple-950
+      <div className="bg-white dark:bg-gradient-to-b from-purple-400 to-purple-900 
         p-8 rounded-xl shadow-lg w-full max-w-md">
 
         <h1 className="text-3xl font-bold text-center mb-6">
@@ -22,7 +22,7 @@ export default function Page() {
           <li>
             <Link
               href="/week-2"
-              className="block text-center p-3 rounded-lg bg-purple-100 text-purple-800 hover:bg-purple-300 hover:text-white dark:bg-purple-900 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+              className="block text-center p-3 rounded-lg bg-purple-100 text-purple-800 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl  dark:bg-purple-900 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
             >
               Week 2
             </Link>
@@ -31,7 +31,7 @@ export default function Page() {
           <li>
             <Link
               href="/week-3"
-              className="block text-center p-3 rounded-lg bg-purple-200 text-purple-900 hover:bg-purple-300 hover:text-white dark:bg-purple-800 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+              className="block text-center p-3 rounded-lg bg-purple-200 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-800 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
             >
               Week 3
             </Link>
@@ -40,7 +40,7 @@ export default function Page() {
           <li>
             <Link
               href="/week-4"
-              className="block text-center p-3 rounded-lg bg-purple-300 text-purple-900 hover:bg-purple-300 hover:text-white dark:bg-purple-700 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+              className="block text-center p-3 rounded-lg bg-purple-300 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-700 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
             >
               Week 4
             </Link>
@@ -49,7 +49,7 @@ export default function Page() {
           <li>
             <Link
               href="/week-5"
-              className="block text-center p-3 rounded-lg bg-purple-400 text-purple-900 hover:bg-purple-300 hover:text-white dark:bg-purple-600 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+              className="block text-center p-3 rounded-lg bg-purple-400 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-600 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
             >
               Week 5
             </Link>
@@ -58,7 +58,7 @@ export default function Page() {
           <li>
             <Link
               href="/week-6"
-              className="block text-center p-3 rounded-lg bg-purple-500 text-purple-900 hover:bg-purple-300 hover:text-white dark:bg-purple-550 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+              className="block text-center p-3 rounded-lg bg-purple-400 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-500 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
             >
               Week 6
             </Link>
@@ -67,9 +67,36 @@ export default function Page() {
           <li>
             <Link
               href="/week-7"
-              className="block text-center p-3 rounded-lg bg-purple-600 text-purple-900 hover:bg-purple-300 hover:text-white dark:bg-purple-500 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+              className="block text-center p-3 rounded-lg bg-purple-500 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-400 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
             >
               Week 7
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/week-8"
+              className="block text-center p-3 rounded-lg bg-purple-500 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-400 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+            >
+              Week 8
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/week-9"
+              className="block text-center p-3 rounded-lg bg-purple-600 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-300 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+            >
+              Week 9
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/week-10"
+              className="block text-center p-3 rounded-lg bg-purple-600 text-purple-900 hover:bg-purple-300 hover:text-white hover:scale-[1.04] transition-all duration-200 hover:shadow-xl dark:bg-purple-300 dark:text-purple-100 dark:hover:bg-purple-300 dark:hover:text-purple-900"
+            >
+              Week 10
             </Link>
           </li>
         </ul>
